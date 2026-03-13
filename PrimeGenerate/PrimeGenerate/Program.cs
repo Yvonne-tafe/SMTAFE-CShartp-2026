a5390559ef9b340bcf6382d260bc1ec798148582
Yvonne-tafe <30138956@tafe.wa.edu.au>
2026-03-13 09:17:27 +0800

    init project PrimeGenerate
    init project PrimeGenerate

---
+++ PrimeGenerate/PrimeGenerate/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PrimeGenerate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create 10 prime numbers and print them to the console
+            int n = 10;
+            PrimeGenerator PG = new PrimeGenerator();
+            int[] PrimeList = PG.Generator(n);
+            for (int i = 0; i < PrimeList.Length; i++)
+            {
+                    Console.WriteLine(PrimeList[i]);
+            }
+        }
+    }
+}
