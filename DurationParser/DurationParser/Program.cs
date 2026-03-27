7a44e7d3c6ca514b3d0afb1255d01336f388fad0
Yvonne-tafe <30138956@tafe.wa.edu.au>
2026-03-27 12:41:43 +0800

    Init the projext DurationParser
    the init files for the project DurationParser. Considering to use a Time Class to instead of array structure.

---
+++ DurationParser/DurationParser/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Xml;
+
+namespace DurationParser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //the format is like "45s", "10m", "1h30m", "2h5m10s"
+            string HumanReadableString = "2h5m10s";
+            int TotalSceond = 0;
+            Console.WriteLine("Human readabele text: " + HumanReadableString);
+            ToSecond(HumanReadableString, out TotalSceond);
+            Console.WriteLine("Total Second: " + TotalSceond.ToString());
+
+            HumanReadableString = "45m";
+            Console.WriteLine("Human readabele text: " + HumanReadableString);
+            ToSecond(HumanReadableString, out TotalSceond);
+            Console.WriteLine("Total Second: " + TotalSceond.ToString());
+
+            HumanReadableString = "10m";
+            Console.WriteLine("Human readabele text: " + HumanReadableString);
+            ToSecond(HumanReadableString, out TotalSceond);
+            Console.WriteLine("Total Second: " + TotalSceond.ToString());
+
+
+            HumanReadableString = "1h30m";
+            Console.WriteLine("Human readabele text: " + HumanReadableString);
+            ToSecond(HumanReadableString, out TotalSceond);
+            Console.WriteLine("Total Second: " + TotalSceond.ToString());
+        }
+
+        //such function has the advance that we can double check the input string,
+        //if the input string is null, we can return false,
+        //and the output parameter will be 0, which is a reasonable default value for total second.
+        //or something wrong with the input string, such as mal formatted string, so we cannot parse it
+        public static bool ToSecond(String? input, out int TotalSceond)
+        {
+            TotalSceond = 0;
+            if (input is null)
+                return false;
+
+            int[] TimeSectionInt = ParseTimeDuration(input);
+            TotalSceond = TimeSectionInt[0]*60*60 + TimeSectionInt[1]*60 + TimeSectionInt[2];
+            return true;
+        }
+        
+        public static int[] ParseTimeDuration(string TimeString)
+        {
+            int[] TimeSection = [0, 0, 0];
+            string[] TimeSlice = ["h", "m", "s"];
+            string ParseString = TimeString;
+            int StartIndex = 0;
+            int CheckIndex = 0;
+
+            for (int i =0; i< TimeSlice.Length; i++)
+            {
+                CheckIndex = ParseString.IndexOf(TimeSlice[i]);
+                TimeSection[i] = CheckIndex > -1
+                    ? int.Parse(ParseString[StartIndex..(CheckIndex)])
+                    : 0; //if the time slice is not found, we can set the corresponding time section to 0, which is a reasonable default value.
+
+                ParseString = ParseString[(CheckIndex + 1)..(ParseString.Length)];
+            }
+            
+            //Console.WriteLine("TimeSection: ");
+            //Console.WriteLine(string.Join(", ", TimeSection.ToString()));
+            return TimeSection;
+
+        }
+    }
+}
---
+++ DurationParser/DurationParser/TimeDuration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DurationParser
+{
+    internal class TimeDuration
+    {
+        public int Hour { get; set; } = 0;
+        public int Minute { get; set; } = 0;
+        public int Second { get; set; } = 0;
+
+        public TimeDuration(int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+
+        public TimeDuration(int second)
+        {
+            Second = second;
+        }
+        public TimeDuration(int minute, int second)
+        {
+            Minute = minute;
+            Second = second;
+        }
+    }
+}
