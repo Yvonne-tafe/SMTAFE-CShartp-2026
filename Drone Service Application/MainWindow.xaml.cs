16b69d7ac2e332053e318af73cf894a124753f38
Yvonne-tafe <30138956@tafe.wa.edu.au>
2026-06-14 16:13:13 +0800

    Reset parameters and added test data
    1. send queues and list data to DisplayQueueWindow
    2. added test data for function testing.

--- Drone Service Application/MainWindow.xaml.cs
+++ Drone Service Application/MainWindow.xaml.cs
@@ -31,6 +31,50 @@ namespace Drone_Service_Application
         {
             InitializeComponent();
             CommonControls.ShowExitButton(false);
+            TestData();
+        }
+        private void TestData()
+        {
+            Drone newDrone = new Drone();
+            newDrone.SetClientName("Test1");
+            newDrone.SetDroneModel("TestModel1");
+            newDrone.SetServiceCost(100);
+            newDrone.SetServiceProblem("Test Problem 1");
+            newDrone.SetServiceTag(100);
+            RegularService.Enqueue(newDrone);
+
+            newDrone = new Drone();
+            newDrone.SetClientName("Test2");
+            newDrone.SetDroneModel("TestModel2");
+            newDrone.SetServiceCost(150);
+            newDrone.SetServiceProblem("Test Problem 2");
+            newDrone.SetServiceTag(110);
+            RegularService.Enqueue(newDrone);
+
+            newDrone = new Drone();
+            newDrone.SetClientName("Test3");
+            newDrone.SetDroneModel("TestModel3");
+            newDrone.SetServiceCost(170);
+            newDrone.SetServiceProblem("Test Problem 3");
+            newDrone.SetServiceTag(120);
+            RegularService.Enqueue(newDrone);
+
+            newDrone = new Drone();
+            newDrone.SetClientName("Test4");
+            newDrone.SetDroneModel("TestModel4");
+            newDrone.SetServiceCost(115);
+            newDrone.SetServiceProblem("Test Problem 4");
+            newDrone.SetServiceTag(130);
+            ExpressService.Enqueue(newDrone);
+
+            newDrone = new Drone();
+            newDrone.SetClientName("Test5");
+            newDrone.SetDroneModel("TestModel5");
+            newDrone.SetServiceCost(175);
+            newDrone.SetServiceProblem("Test Problem 5");
+            newDrone.SetServiceTag(140);
+            ExpressService.Enqueue(newDrone);
+            nextServiceTag = 150;
         }
 
         //Click to Add new drone item
@@ -47,7 +91,7 @@ namespace Drone_Service_Application
         //6.13	Create a mouse click method for the express service ListView that will display the Client Name and Service Problem in the related textboxes.
         private void Button_Click_ShowQueues(object sender, RoutedEventArgs e)
         {
-            DisplayQueueWindow addWindow = new DisplayQueueWindow();
+            DisplayQueueWindow addWindow = new DisplayQueueWindow(RegularService, ExpressService, FinishedList);
             addWindow.ShowDialog();
         }
 
